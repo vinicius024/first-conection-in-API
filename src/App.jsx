a21933314dc489879e677ce86511b0d02726537f
vinicius <viniciusfreitassilva024@gmail.com>
2022-12-26 20:39:34 -0300

    projeto de conecção com API

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import './styles.css'
+
+function App() {
+
+  const [nutri, setNutri] = useState([]);
+
+
+  useEffect(() => {
+
+    function loadApi(){
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+      fetch(url)
+      .then( (result) => result.json())
+      .then( (json) => {
+        console.log(json);
+        setNutri(json);
+      })
+    }
+
+    loadApi();
+  }, []);
+
+
+  return (
+    <div className='container'>
+      <header>
+        <strong>React Nutri</strong>
+      </header>
+
+      {nutri.map( (item) => {
+        return(
+          <article key={item.id} className="post">
+            <strong className='titulo'>{item.titulo}</strong>
+            <img src={item.capa} alt={item.titulo} className='capa'/>
+            <p className='subtitulo'>{item.subtitulo}</p>
+            <a className='button'>Acessar</a>
+          </article>
+        );
+      })}
+    </div>
+  )
+}
+
+export default App
